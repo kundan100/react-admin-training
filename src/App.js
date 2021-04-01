41be800843e555f7f6192bc6cd09d5bc81fd3741
kundan100 <kundan100@yahoo.com>
2021-04-01 18:15:30 +0530

    add Resource component to display the list (users)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Admin } from 'react-admin';
+import {Admin, Resource, ListGuesser} from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
@@ -7,7 +7,9 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 const App = () => {
   return(
     <div className="App">
-      <Admin dataProvider={dataProvider} />
+      <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={ListGuesser} />
+      </Admin>
     </div>
   )
 }
